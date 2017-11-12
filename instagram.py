e48f0c77daeddf1dc813ea0c4473316dcb2c9186
Betepok <betepoknoname@gmail.com>
2017-11-12 13:46:21 +0300

    fixed issue with id in graphql request

--- instagram.py
+++ instagram.py
@@ -3,7 +3,7 @@ from json import loads as json_decode
 import datetime
 from time import sleep
 
-location = 97755147
+location = 267646263
 
 response = requests.get('https://www.instagram.com/explore/locations/%d/?__a=1' % location)
 data = json_decode(response.text)
@@ -16,7 +16,7 @@ print 'long: %f' % data['location']['lng']
 next_page = data['location']['media']['page_info']['end_cursor']
 
 while True:
-	response = requests.get('https://www.instagram.com/graphql/query/?query_id=17865274345132052&variables={"id":"1639313","first":12,"after":"%s"}' % next_page)
+	response = requests.get('https://www.instagram.com/graphql/query/?query_id=17865274345132052&variables={"id":"%d","first":12,"after":"%s"}' % (location, next_page))
 	data = json_decode(response.text)
 	if data['data']['location']['edge_location_to_media']['page_info']['has_next_page'] == False:
 		print 'There are no more pages'
